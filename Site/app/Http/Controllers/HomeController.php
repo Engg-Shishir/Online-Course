0aa755c4c134478f95f47fbba7881c40cf323ad6
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2021-04-15 23:50:41 +0600

    Site show service in home page

--- Site/app/Http/Controllers/HomeController.php
+++ Site/app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\VisitorModel;
+use App\Models\Service;
 
 
 class HomeController extends Controller
@@ -16,9 +17,12 @@ class HomeController extends Controller
         date_default_timezone_set("Asia/Dhaka");
         $timeDate= date("Y-m-d h:i:sa");
 
+        // Insert User Ip Address in database
         VisitorModel::insert(['ip_address'=>$UserIP,'visit_time'=>$timeDate]);
 
-        return view('home');
+        // Get All Service data and show in home page
+        $serviceData = json_decode(Service::all(),true);
+        return view('home',['serviceData'=>$serviceData]);
 
     }
 }
