40fa905d23c2016de6c1c4f7c82376d0fe95e8b4
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2021-04-25 16:40:16 +0600

    Admin Login System with Middleware

--- Admin/routes/web.php
+++ Admin/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\VisitorController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\CourseCntroller;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,25 +19,25 @@ use App\Http\Controllers\CourseCntroller;
 */
 
 // Show Admin Home page
-Route::get('/',[HomeController::class,'HomeIndex']);
+Route::get('/',[HomeController::class,'HomeIndex'])->middleware('loginCheck');
 
 // Show Admin Visitor page
-Route::get('/visitor',[VisitorController::class,'VisitorIndex']);
+Route::get('/visitor',[VisitorController::class,'VisitorIndex'])->middleware('loginCheck');
 
 
 
 // Show Admin Service page
-Route::get('/service',[ServiceController::class,'ServiceIndex']);
+Route::get('/service',[ServiceController::class,'ServiceIndex'])->middleware('loginCheck');
 // Show all service data from database
-Route::get('/getServiceData',[ServiceController::class,'GetServiceData']);
+Route::get('/getServiceData',[ServiceController::class,'GetServiceData'])->middleware('loginCheck');
 // Add New Service
-Route::post('/addService',[ServiceController::class,'addService']);
+Route::post('/addService',[ServiceController::class,'addService'])->middleware('loginCheck');
 // Delete Service
-Route::post('/serviceDelete',[ServiceController::class,'serviceDelete']);
+Route::post('/serviceDelete',[ServiceController::class,'serviceDelete'])->middleware('loginCheck');
 // Get service details
-Route::post('/serviceDetails',[ServiceController::class,'getServiceDetails']);
+Route::post('/serviceDetails',[ServiceController::class,'getServiceDetails'])->middleware('loginCheck');
 // Update service data
-Route::post('/serviceUpdate',[ServiceController::class,'serviceUpdate']);
+Route::post('/serviceUpdate',[ServiceController::class,'serviceUpdate'])->middleware('loginCheck');
 
 
 
@@ -44,23 +45,26 @@ Route::post('/serviceUpdate',[ServiceController::class,'serviceUpdate']);
 
 
 // Show Admin Service page
-Route::get('/course',[CourseCntroller::class,'CourseIndex']);
+Route::get('/course',[CourseCntroller::class,'CourseIndex'])->middleware('loginCheck');
 // Show all service data from database
-Route::get('/getCourseData',[CourseCntroller::class,'getCourseData']);
+Route::get('/getCourseData',[CourseCntroller::class,'getCourseData'])->middleware('loginCheck');
 // Add New Service
-Route::post('/addCourse',[CourseCntroller::class,'addCourse']);
+Route::post('/addCourse',[CourseCntroller::class,'addCourse'])->middleware('loginCheck');
 // Delete Service
-Route::post('/courseDelete',[CourseCntroller::class,'courseDelete']);
+Route::post('/courseDelete',[CourseCntroller::class,'courseDelete'])->middleware('loginCheck');
 // Get service details
-Route::post('/courseDetails',[CourseCntroller::class,'getCourseDetails']);
+Route::post('/courseDetails',[CourseCntroller::class,'getCourseDetails'])->middleware('loginCheck');
 // Update service data
-Route::post('/courseUpdate',[CourseCntroller::class,'courseUpdate']);
+Route::post('/courseUpdate',[CourseCntroller::class,'courseUpdate'])->middleware('loginCheck');
 
 
 
-// Admin Login Form
+
+
+// Admin Panel Review Management
 Route::get('/Login', [LoginController::class,'LoginIndex']);
-// Try Admin Login 
+// Admin Panel Review Management
 Route::post('/onLogin', [LoginController::class,'onLogin']);
-// Admin Logout
-Route::get('/Logout', [LoginController::class,'onLogout']);
+// Admin Panel Review Management
+Route::get('/Logout', [LoginController::class,'onLogout'])->middleware('loginCheck');
+
